644029c9f8f4c370efbe9a6479847fb29633134f
= <=>
2020-02-01 17:49:27 -0500

    Adds readme.md

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,4 @@ import * as ReactDOM from 'react-dom';
 
 import { App } from './components/App';
 
-
-
 ReactDOM.render(<App compiler="TypeScript" framework="react" />, document.querySelector('#root'));
